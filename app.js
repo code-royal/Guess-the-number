418d28b4e0c468eecca6bff82a0727b1236d84c7
Prajwal Rajbhandari <prajwalrajbhandari8@gmail.com>
2026-04-27 21:58:54 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var guessInput = document.getElementById('guessInput');
+var checkBtn = document.getElementById('check');
+var restartBtn = document.getElementById('restart');
+var resultDisplay = document.getElementById('result');
+
+var randomNum = Math.floor(Math.random() * 100) + 1; //initializes random number from 1 to 100
+
+// event handling for check button
+checkBtn.addEventListener('click',()=>{
+    var guess = Number(guessInput.value.trim());
+
+    if(guess<1 || guess>100){
+        resultDisplay.textContent = "Enter numbers from 1 to 100."; //if the user enters invalid numbers
+        resultDisplay.style.color = "#c81d25";
+    }
+    else if(guess === randomNum){
+        resultDisplay.textContent = "Congrats!!🥳 You guessed it right."; //if the user guessed it right
+        resultDisplay.style.color = "#008000";
+        checkBtn.style.display = "none"; //hides check button
+        restartBtn.style.display = "inline-block"; //unhides restart button
+    }
+    else {
+        resultDisplay.textContent = guess < randomNum ? "Guess a higher number!!" : "Guess a lower number!!";
+        resultDisplay.style.color = "red";
+    }
+
+    guessInput.value = "";  //resets the input field
+    guessInput.focus(); //keeps the focus on the input field
+})
+
+restartBtn.addEventListener('click', ()=>{
+        randomNum = Math.floor(Math.random() * 100) + 1;
+        resultDisplay.textContent = "";
+        restartBtn.style.display = "none"; //hides restart button
+        checkBtn.style.display = "inline-block"; //unhides check button
+        guessInput.focus();
+    })
